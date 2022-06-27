8492793f63b15c940b571b3974b1c0f2bc2f164c
Damien Richcreek <damien@richcreek.dev>
2022-06-27 08:03:24 -0700

    reset to  original commit, all tasks complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,51 +1,213 @@
-// ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
+/* ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
+Please read all questions thoroughly
+Pseudo coding is REQUIRED
+If you get stuck, please leave comments to help us understand your thought process
 
-// Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
+Use test driven development to complete the following questions
+Add appropriate dependencies: $ yarn add jest
 
-// Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
+Reminder: The test will call your function
+Run the file with the following command: $ yarn jest
 
-// --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+/* ---------------- Question 1 --------------- */
+/* 1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized. */
+/* ------------------------------------------- */
 
-// a) Create a test with an expect statement using the variable provided.
+  // a) Create a test with an expect statement using the variable provided.
+  describe("intros", () => {
+    it("takes in an array of objects and returns an array with a sentance about each person, with their name capitalized", () => {
+      const people = [
+        { name: "ford prefect", occupation: "a hitchhiker" },
+        { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+        { name: "arthur dent", occupation: "a radio employee" }
+      ]
+      const expected = ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+      expect(intros(people)).toEqual(expected)
+    })
+  })
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+  /* Red Test Failed Successfully
+  FAIL  ./code-challenges.test.js
+  intros
+    ✕ takes in an array of objects and returns an array with a sentance about each person, with their name capitalized (1 ms)
+    ReferenceError: intros is not defined
+  */
 
+  // b) Create the function that makes the test pass.
 
-// b) Create the function that makes the test pass.
+  /* Pseudocode - intros
+      Function: name = intros
+      Parameters: 1 x array of objects, name = arrOfObjects
+      Logic:
+        - create a variable to store introduction string array named introStrArr
+        - iterate over each object assigning the stringified sentance of the name and occupation
+        - within the iteration take the names and split them into an array of single words and assign it to a variable named names
+        - iterate over the array of names replacing the first letter with a capitalized version
+        - take capitalized first and last names and assign them to a variable named capName
+        - return the concatenated capName variable with 'is' separating the occupation and end with a period
+        - then return the introStrArr
+  */
 
+  // const intros = (arrOfobjects) => {
+  //   const introStrArr = arrOfobjects.map(person =>{
+  //     let names = person.name.split(" ")
+  //     for(var i = 0; i < names.length; i++) {
+  //       names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1)
+  //     }
+  //     let capName = names.join(" ")
+  //     return `${capName} is ${person.occupation}.`
+  //   })
+  //   return introStrArr
+  // }
 
-// --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+  /* Green Test Passed Successfully
+       PASS  ./code-challenges.test.js
+    intros
+      ✓ takes in an array of objects and returns an array with a sentance about each person, with their name capitalized (2 ms)
+      Test Suites: 1 passed, 1 total
+  */
+
+  /* Refactor */
+  const intros = arrOfobjects => {
+    return introStrArr = arrOfobjects.map(person =>{
+      fullName = person.name.split(" ").map(nameStr => {
+        return nameStr.charAt(0).toUpperCase() + nameStr.slice(1)
+      }).join(" ")
+      return `${fullName} is ${person.occupation}.`
+    })
+  }
+
+/* ---------------- Question 2 --------------- */
+/* Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. */
+/* ------------------------------------------- */
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe('divByThree', () => {
+  it('take in a mixed data array and returns an array of only the remainders of the numbers when devided by 3', () => {
+    // Test Inputs
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    // Expected Results
+    const expected1 = [ 2, 0, -1, 0 ]
+    const expected2 = [ 2, 1, -1 ]
+    
+    expect(divByThree(hodgepodge1)).toEqual(expected1)
+    expect(divByThree(hodgepodge2)).toEqual(expected2)
+  });
+});
 
+    /* Red Test Failed Successfully
+        FAIL  ./code-challenges.test.js
+        intros
+          ✓ takes in an array of objects and returns an array with a sentance about each person, with their name capitalized (2 ms)
+        divByThree
+          ✕ take in a mixed data array and returns an array of only the remainders of the numbers when devided by 3 (1 ms)
+          ReferenceError: divByThree is not defined
+    */
 
 // b) Create the function that makes the test pass.
 
+/* Pseudocode - divByThree
+  Function: divByThree
+  Parameters: 1 x mixed data array named mixArr
+  Logic:
+    - Create variable called nums to contain all numbers
+    - Iterate over each item, moving only the numbers to the nums array
+    - If item is a number, divide by three and return remainder to nums arr
+    - Once all items have been iterated over, return newly formed arr of remainder numbers that have been computed
+*/
 
-// --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
+  // const divByThree = (mixArr) => {
+  //   const nums = mixArr.filter(item => {
+  //     return typeof item === "number"
+  //   })
+
+  //   divNums = nums.map(num => {
+  //     return num % 3
+  //   })
+  //   return divNums
+  // }
+
+  /* Green Test Passed Successfully
+    PASS  ./code-challenges.test.js
+    intros
+      ✓ takes in an array of objects and returns an array with a sentance about each person, with their name capitalized (3 ms)
+    divByThree
+      ✓ take in a mixed data array and returns an array of only the remainders of the numbers when devided by 3 (1 ms)
+  */
+
+  /* Refactor */
+  const divByThree = mixArr => {
+    filter: nums    = mixArr.filter(item => typeof item === "number")
+    map:    divNums = nums.map(num => num % 3)
+    return divNums
+  }
+
+
+/* ---------------- Question 3 --------------- */
+/* 3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed. */
+/* ------------------------------------------- */
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+describe('cubeIt', () => {
+  it('takes in an array of numbers and returns the sum of all the numbers cubed', () => {
+    const cubeAndSum1 = [2, 3, 4]
+    const cubeAndSum2 = [0, 5, 10]
+    const expected1 = 99
+    const expected2 = 1125
+    expect(cubeIt(cubeAndSum1)).toEqual(expected1)
+    expect(cubeIt(cubeAndSum2)).toEqual(expected2)
+  });
+});
 
+/* Red Test Failed Successfully
+   FAIL  ./code-challenges.test.js
+  intros
+    ✓ takes in an array of objects and returns an array with a sentance about each person, with their name capitalized (3 ms)
+  divByThree
+    ✓ take in a mixed data array and returns an array of only the remainders of the numbers when devided by 3 (1 ms)
+  cubeIt
+    ✕ takes in an array of numbers and returns the sum of all the numbers cubed (1 ms)
+  ReferenceError: cubeIt is not defined
+*/
 
 // b) Create the function that makes the test pass.
+
+  /* Pseudocode - cubeIt
+    Function: cubeIt
+    Parameters: 1 x array of numbers (numArr)
+    Logic:
+      - create a new variable called cubedNums
+      - iterate over each number in the numsArr
+      - return the number cubed for each number back to the cubedNums array
+      - after all numbers have been cubed, iterate over the numbers
+      - add the value to the sum of the last number until all numbers have been sumed up
+      - return the sumed value of all the numbers
+  */
+
+  // const cubeIt = (numArr) => {
+  //   const cubedNums = numArr.map(num => {
+  //     return num ** 3
+  //   })
+  //   return cubedNums.reduce((num1, num2) => num1 + num2, 0)
+  // }
+
+  /* Green Test Passed Successfully
+      PASS  ./code-challenges.test.js
+    intros
+      ✓ takes in an array of objects and returns an array with a sentance about each person, with their name capitalized (3 ms)
+    divByThree
+      ✓ take in a mixed data array and returns an array of only the remainders of the numbers when devided by 3
+    cubeIt
+      ✓ takes in an array of numbers and returns the sum of all the numbers cubed (1 ms)
+    Test Suites: 1 passed, 1 total
+  */
+
+  /* Refactor */
+  const cubeIt = numArr => {
+    cubed: cubedNums = numArr.map(num => num ** 3)
+    sum: return cubedNums.reduce((num1, num2) => num1 + num2, 0)
+  }
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) This line defines a new class controller with a name of BlogPostsController inherrited from the ApplicationController class object
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) This line declares an instance variable named posts which equals all records in the BlogPost table (query finds all records)
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) This line defines the show action method for the individual blog posts
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) This line defines the new action method for creating the view to create a new blog post
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) This line declares a new instance variable named post, which equals a new record in the BlogPost table, after the params passed in have been varified by blog_post_params (strong params)
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,13 +34,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6) This line declares an instance variable named post thats value equals a single BlogPost record, found by using the id of the record
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) This line declares an instance variable named post thats value is equal to an updated post record, it's arguements are passed into the blog_post_params to ensure the items inputted are allowed before updating the record
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8) This line redirects the user to the individual blog post path if the blog post id passed in isn't able to delete the record (ie Record doesn't exist or Error in destroying the record)
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) The private keyword allows all code below the line to not be accessible to anything other than the methods in this class object.
   private
   def blog_post_params
-    # ---10)
+    # ---10) This line takes in a table (converted to a snake case symbol) and provides attributes which should be permitted
     params.require(:blog_post).permit(:title, :content)
   end
 end
